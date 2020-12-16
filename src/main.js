ac44e04507a676b1e10ae9db1c3a1446ae564e5c
beinganushasan <beinganushasan@gmail.com>
2020-12-16 18:23:16 +0545

    added axios for getting data from github

--- src/main.js
+++ src/main.js
@@ -2,21 +2,15 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-// import VueAnalytics from 'vue-analytics'
+import Axios from "axios";
+
 
 Vue.config.productionTip = false
 // const isProd = process.env.NODE_ENV === 'production'
+Vue.prototype.$axios = Axios;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
 
-// Vue.use(VueAnalytics, {
-//   id: 'UA-139190314-1',
-//   router,
-//   debug: {
-//     enabled: !isProd,
-//     sendHitTask: isProd
-//   }
-// })
